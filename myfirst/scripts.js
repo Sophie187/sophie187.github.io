1a2495096f09f5fd88ab689ff2e286c0dc084ade
Sophie187 <66476869+Sophie187@users.noreply.github.com>
2021-03-26 10:06:50 +0100

    myfirst

--- myfirst/scripts.js
+++ myfirst/scripts.js
@@ -2,12 +2,12 @@ let currentphoto = 0
 let imagesData = [{
         photo: 'images/jellyfish.jpg',
         title: 'Jellyfish',
-        description: 'What happened here, why is this a very nice image'
+        description: 'Fascinating, elegant, and mysterious to watch in the water, take a jellyfish out of the water, and it becomes a much less fascinating blob. This is because jellyfish are about 95 percent water.'
     },
     {
         photo: 'images/jellyfish2.jpg',
         title: 'Jellyfish2',
-        description: 'What happened here, why is this a very nice image'
+        description: 'There are almost 200 known species of jellyfish drifting through our planet’s seas. While many of them are actually quite harmless, there are several species that have highly venomous stinging cells that can cause serious consequences for humans—such as extreme pain or even death.'
     },
     {
         photo: 'images/jellyfish3.jpg',
@@ -38,8 +38,16 @@ $('#jellyfish').attr('src', imagesData[currentphoto].photo);
 let loadPhoto = (photoNumber) => {
     $('#jellyfish').attr('src', imagesData[photoNumber].photo);
     $('#jellyfish').css('background-image', 'url(' + imagesData[photoNumber].photo + ')');
-    // ...
-}
+    $('.description').text(imagesData[photoNumber].description)
+
+
+
+    $('li').removeClass("active")
+    $('li').eq(photoNumber).addClass("active")
+
+};
+// ...
+
 
 loadPhoto(currentphoto);
 $('.arrow-right').click(() => {
@@ -60,11 +68,19 @@ let thumbnails = ['Jellyfish', 'Jellyfish2', 'Jellyfish3']
 
 imagesData.forEach((item, index) => {
     $('.thumbnailslist').append(`<li target="_blank" href="images/jellyfish.jpg">
-            <img class="thumbnailsimg"  data-index="${index}" src="${item.photo}" alt="jellyfish"></li>`);
+    <div class="tooltip">Chironex Fleckeri</div>
+     <div class="thumbnails"</div>
+            <img class="thumbnailsimg"  data-index="${index}" src="${item.photo}" alt="jellyfish"></li>`
+
+    );
+
+
 })
 $('.thumbnailsimg').click((event) => {
     let indexClicked = $(event.target).attr('data-index');
     console.log(indexClicked)
     loadPhoto(indexClicked)
+
+
 
 })
